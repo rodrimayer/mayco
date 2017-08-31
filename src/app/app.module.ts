5ded893b1939ebf580988faf1c649789f95aab26
nicolas <nicolasbloj@gmail.com>
2017-08-31 01:04:51 -0300

    config and core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { CoreModule } from 'app/core/core.module';
+import { ShellComponent } from 'app/core/shell/shell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ShellComponent]
 })
 export class AppModule { }
